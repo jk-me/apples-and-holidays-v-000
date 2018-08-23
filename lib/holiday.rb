a63809755a187c6d47634c967e682e295c7316c1
Jenny K <jkam17@gmail.com>
2018-08-23 03:17:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,13 +62,19 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each{ |season,hhash|
     puts "#{season}:"
     hhash.each{ |holiday,suparray|
-    puts "#{holiday}: #{"
+    puts "#{holiday}: #{suparray.join(, )}"
+    }
+  }
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.each{ |season,hhash|
+    hhash.each{ |holiday,suparray|
+    puts "#{holiday}: #{suparray.join(, )}"
+    }
+  }  
 end
 
 
