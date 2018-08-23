44ffc0009a95ac471070aba88441707585305da5
Jenny K <jkam17@gmail.com>
2018-08-23 03:28:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     hhash.each{ |holiday,suparray|
       n=holiday.to_s.split('_')
-      n.each{|x| x.capitalize}
+      n=n.collect{|x| x.capitalize}
       n=n.join(' ')
       puts "  #{n}: #{suparray.join(', ')}"
       }
